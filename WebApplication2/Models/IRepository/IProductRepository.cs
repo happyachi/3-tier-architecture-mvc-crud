08f7a2d0f2b2eb8af6e882ba261888b40591cd19
happyachi <gn77233@gmail.com>
2024-01-14 11:15:58 +0800

    新增Interface

---
+++ WebApplication2/Models/IRepository/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication2.Models.Core;
+using WebApplication2.Models.EFModels;
+
+namespace WebApplication2.Models.IRepository
+{
+	public interface IProductRepository
+	{
+		void Create(ProductEntity model);
+		void Delete(int id);
+		void Update(ProductEntity entity);
+	}
+}
